a2380e3799065a5d607af1ae09ec51c421cfd17e
rsgoheen <rsgoheen@rsgoheen-Ubuntu.(none)>
2013-07-09 10:57:17 +0100

    Adding reading of Index.html and writing to page

--- web.js
+++ web.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	fs.readFile('./index.html',
+		function(err, data) {
+			var buf = new Buffer(data);
+			console.log("Read data " + data);
+			console.log("Read buffer " + buf.toString());
+
+			response.send(buf.toString());
+		});
 });
+
+function writeResponse(data) {
+	response.send(data);
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
